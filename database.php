fb7f73a8ad43993998edd9bef94d9df4f1637d97
Pyckselgas <61231059+Pyckselgas@users.noreply.github.com>
2021-08-27 15:03:03 +0300

    dumped

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-$par1_ip = "sin.db0sel.intra" ;
+$par1_ip = "dsp2s.dt07.net" ;
 $par2_name = "disportnews" ;
 $par3_password = "4sr9NBnC3H7OtfoL" ;
 $par4_db = "disportnews" ;
@@ -8,7 +8,7 @@ $par4_db = "disportnews" ;
 $induction = mysqli_connect($par1_ip, $par2_name, $par3_password, $par4_db) ;
 
 if ($induction == false){
-echo "No connection to database"
+echo "No connection to database";
 }
 
 ?>
